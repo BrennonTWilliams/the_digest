f71428f39ebeace6bd50a5b0f734460ad4c9898c
Myles Gleeson <mylesgleeson@live.com>
2015-10-17 14:08:44 +1100

    Added subscribed column to User schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150915123156) do
+ActiveRecord::Schema.define(version: 20151017023909) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20150915123156) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.boolean  "subscribed"
   end
 
 end
