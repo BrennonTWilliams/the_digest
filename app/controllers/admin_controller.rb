6257347e101a66a875384471337bff8740247981
Myles Gleeson <mylesgleeson@live.com>
2015-10-18 17:12:08 +1100

    Set up the Mandrill API key and email function so that the admin/email path will send an email to all subscribed users (at the moment just a generic message)

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,11 +1,41 @@
 class AdminController < ApplicationController
 
+  require 'mandrill'
+
+  def getSubscribedUsers
   
+    User.where(subscribed: true).to_a
+  
+  end
 
   
   
   def email
     
+    Excon.defaults[:ssl_verify_peer] = false
+    @sendings = []
+    subscribers = getSubscribedUsers
+    subscribers.each do |subscriber|
+      m = Mandrill::API.new
+      message = {
+        :subject => 'Your news digest!',
+        :from_name => 'The News Digest',
+        :text => 'This is an email send with Mandril!!',
+        :to => [
+            {
+              :email => subscriber.email,
+              :name => '#{subscriber.firstname} #{subscriber.lastname}'
+              
+            }
+          ],
+          :html => 'This is your news digest',
+          :from_email => 'admin@NewsDigest.com'
+      }
+      
+      sending = m.messages.send message
+      @sendings << sending
+      
+    end
   
   end
   
