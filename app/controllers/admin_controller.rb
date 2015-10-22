8beebf9e9b84afc5b4fcb18a186075121d9f4fc6
Myles Gleeson <mylesgleeson@live.com>
2015-10-22 15:48:36 +1100

    Added check so that subscribers will not recieve the same article in a news digest twice

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -18,10 +18,16 @@ class AdminController < ApplicationController
     subscribers.each do |subscriber|
       
       
+      # Get all articles which share any of the tags in the
+      # subscriber's interest list
       articles = Article.tagged_with(subscriber.interest_list, :any => true).to_a
+      # Only use the articles which have not been sent to this subscriber before
+      sent_articles = subscriber.articles.to_a
+      articles = articles - sent_articles
+      
       
       if articles.count == 0
-        
+            
         message_body = 'It appears that there are no recent articles which match your interests.'
         message_body += '<br>Would you like to add some interests to your News Digest profile?'
       
@@ -29,6 +35,11 @@ class AdminController < ApplicationController
         
         message_body = 'Here is your news Digest!<br><br>'
         articles.first(10).each do |article|
+          
+          # Add an entry to articles/users join table so that the
+          # article will not be sent to this user again.
+          subscriber.articles << article
+          
           message_body += '<br><strong>Title</strong>'
           message_body += "<br>#{article.title}"
           message_body += '<br><strong>Author</strong>'
