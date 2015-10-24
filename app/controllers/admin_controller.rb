4141c136710af7f5200ad02d6c2950430c0e3fea
Myles Gleeson <mylesgleeson@live.com>
2015-10-24 12:50:37 +1100

    Moved some of the code from the admin_controller to the admin_helper, to avoid having buisness logic in the contoller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,8 @@
 class AdminController < ApplicationController
 
+  include AdminHelper
   require 'mandrill'
+  
 
   def getSubscribedUsers
   
@@ -12,70 +14,15 @@ class AdminController < ApplicationController
   
   def email
     
-    Excon.defaults[:ssl_verify_peer] = false
+    
     @sendings = []
     subscribers = getSubscribedUsers
     subscribers.each do |subscriber|
       
-      
-      # Get all articles which share any of the tags in the
-      # subscriber's interest list
-      articles = Article.tagged_with(subscriber.interest_list, :any => true).to_a
-      # Only use the articles which have not been sent to this subscriber before
-      sent_articles = subscriber.articles.to_a
-      articles = articles - sent_articles
-      
-      # Sort the articles to show the most recent first
-      articles.sort_by! {|a| a.pubDate }.reverse!
-      
-      
-      if articles.count == 0
-            
-        message_body = 'It appears that there are no recent articles which match your interests.'
-        message_body += '<br>Would you like to add some interests to your News Digest profile?'
-      
-      else
-        
-        message_body = 'Here is your news Digest!<br><br>'
-        articles.first(10).each do |article|
-          
-          # Add an entry to articles/users join table so that the
-          # article will not be sent to this user again.
-          subscriber.articles << article
-          
-          message_body += '<br><strong>Title</strong>'
-          message_body += "<br>#{article.title}"
-          message_body += '<br><strong>Author</strong>'
-          message_body += "<br>#{article.author}"
-          message_body += "<br><a href='http://localhost:3000/articles/#{article.id}'>Show More</a>"
-          message_body += '<br><br>'
-        
-        end
-      
-      end
-      
-      
-      
-      m = Mandrill::API.new
-      message = {
-        :subject => 'Your news digest!',
-        :from_name => 'The News Digest',
-        :text => 'This is an email send with Mandril!!',
-        :to => [
-            {
-              :email => subscriber.email,
-              :name => "#{subscriber.first_name} #{subscriber.last_name}"
-              
-            }
-          ],
-          :html => message_body,
-          :from_email => 'admin@NewsDigest.com'
-      }
-      
-      sending = m.messages.send message
-      @sendings << sending
+      @sendings << email_digest(subscriber)
       
     end
+  
   
   end
   
--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,2 +1,80 @@
 module AdminHelper
+
+  def email_digest(subscriber)
+  
+  
+  
+    # Get all articles which share any of the tags in the
+    # subscriber's interest list
+    articles = Article.tagged_with(subscriber.interest_list, :any => true).to_a
+    # Only use the articles which have not been sent to this subscriber before
+    sent_articles = subscriber.articles.to_a
+    articles = articles - sent_articles
+    # Sort the articles to show the most recent first
+    articles.sort_by! {|a| a.pubDate }.reverse!
+  
+    if articles.count == 0
+      
+      message_body = 'It appears that there are no recent articles which match your interests.'
+      message_body += '<br>Would you like to add some interests to your News Digest profile?'
+      
+    else
+        
+      message_body = 'Here is your news Digest!<br><br>'
+      articles.first(10).each do |article|
+          
+        # Add an entry to articles/users join table so that the
+        # article will not be sent to this user again.
+        subscriber.articles << article
+        
+        message_body += article_email_text(article)
+        
+      end
+      
+    end
+  
+    # Send the message using the Madrill API.
+    # Mandrill depends on a valid API key being set to the environment
+    # variable MANDRILL_APIKEY. This is set in the file config/local_env.yml
+    m = Mandrill::API.new
+    Excon.defaults[:ssl_verify_peer] = false
+    message = {
+      :subject => 'Your news digest!',
+      :from_name => 'The News Digest',
+      :text => 'This is an email send with Mandril!!',
+      :to => [
+          {
+            :email => subscriber.email,
+            :name => "#{subscriber.first_name} #{subscriber.last_name}"
+            
+          }
+        ],
+        :html => message_body,
+        :from_email => 'admin@NewsDigest.com'
+    }
+    
+    sending = m.messages.send message
+    
+  
+  end
+
+
+  def article_email_text(article)
+  
+    text = '<br><strong>Title</strong>'
+    text += "<br>#{article.title}"
+    text += '<br><strong>Author</strong>'
+    text += "<br>#{article.author}"
+    text += '<br><strong>PubDate</strong></br>'
+    text += "<br>#{article.pubDate}"
+    text += "<br><a href='http://localhost:3000/articles/#{article.id}'>Show More</a>"
+    text += '<br><br>'
+  
+  end
+  
+  
+  
+  
+
+
 end
