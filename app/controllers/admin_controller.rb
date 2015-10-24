5d8ab156020cde29a711e9db4d25269860e4ee57
Myles Gleeson <mylesgleeson@live.com>
2015-10-24 13:24:33 +1100

    Added comments to admin_controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -11,24 +11,20 @@ class AdminController < ApplicationController
   end
 
   
-  
+  # Email a news digest to all subscribed users
   def email
-    
     
     @sendings = []
     subscribers = getSubscribedUsers
     subscribers.each do |subscriber|
-      
+      # Email the digest to each subscriber, and record
+      # the Mandrill response in @sendings
       @sendings << email_digest(subscriber)
       
     end
   
-  
   end
   
-  
-  
-
 
 
 end
